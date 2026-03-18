a679545fedede05b27e6838bf60470fb2a210b6a
ethan2dn <ethan@bemyfriends.com>
2026-03-18 14:05:52 +0900

    Updates

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../src/stories/**/*.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y"
+  ],
+  framework: "@storybook/react-vite"
+};
+export default config;
